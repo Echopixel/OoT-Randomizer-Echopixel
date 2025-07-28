9b03da2ad21edbc5a05061780b1642b2382a8742
Fenhl <fenhl@fenhl.net>
2025-07-28 23:38:20 +0000

    Code cleanup

--- ASM/src/hacks/ovl_en_okarina_tag.asm
+++ ASM/src/hacks/ovl_en_okarina_tag.asm
@@ -3,14 +3,14 @@
 .headersize(0x80A86C00 - 0xE09540)
 
 .org 0x80a87260
-;   Replaces:
-;   lui        t4,0x80a8
-;   or         t3,t2,at
-;   sw         t3,0x670(t0)
-;   addiu      t4,t4,0x72d0
-    or          t3, t2, at
-    sw          t3, 0x670(t0)
-    li          t4, EnOkarinaTag_ActionHook
+; Replaces:
+;   lui     t4,0x80a8
+;   or      t3,t2,at
+;   sw      t3,0x670(t0)
+;   addiu   t4,t4,0x72d0
+    or      t3, t2, at
+    sw      t3, 0x670(t0)
+    li      t4, EnOkarinaTag_ActionHook
 
 ; Patch relocs
 .org 0x80a8806c
